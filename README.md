35d73c2b1afc914aef57cd7840746873c6de1931
mertsimsek <mrtsmsk1@gmail.com>
2015-06-24 01:32:59 +0300

    Settings removed.

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # DotLineWatchface
 Android wear watchface
+[Available on Play Store](https://play.google.com/store/apps/details?id=co.mobiwise.dotlinewatchface)
 
 ![alt tag](http://i62.tinypic.com/2wp14rp.png)
--- app/src/main/java/co/mobiwise/dotlinewatchface/WearControlActivity.java
+++ app/src/main/java/co/mobiwise/dotlinewatchface/WearControlActivity.java
@@ -69,28 +69,6 @@ public class WearControlActivity extends ActionBarActivity implements MyRecycler
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_wear_control, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     private int[] getDefaultColors(){
         int[] colorset = getApplicationContext().getResources().getIntArray(R.array.colorset);
         return colorset;
